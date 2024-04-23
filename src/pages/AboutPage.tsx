130390a36270864ead5a1cd1b0dbb7dbf2b62542
Ida <ida.nacka@gmail.com>
2024-04-23 23:04:24 +0200

    updated styling for about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,12 +1,41 @@
 import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 import "../scss/aboutPage.scss";
+import "../scss/section.scss";
+import { useTranslation } from "react-i18next";
 
 export const AboutPage = () => {
+  const [t] = useTranslation("global");
+
   return (
     <>
       <Header />
-      <main></main>
-      <footer></footer>
+      <main className="about-container">
+        <h1 className="header-title">{t("headers.aboutTitle")}</h1>
+        <div className="about-me">
+          <div className="portrait-container">
+            <img className="portrait" src="" alt="hej" />
+          </div>
+          <div className="about-me-container">
+            <h2 className="about-me-title">{t("headers.homeTitle")}</h2>
+            <p className="about-me-text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga
+              natus quisquam nemo dicta non? Quibusdam nulla et ullam illo at.
+              Doloremque beatae optio dignissimos eum quisquam temporibus ipsa
+              cumque enim. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Tempora eos delectus unde harum hic.
+            </p>
+            <a
+              href="path/to/your/file.pdf"
+              download="filename.pdf"
+              className="download-link"
+            >
+              Download CV
+            </a>
+          </div>
+        </div>
+      </main>
+      <Footer />
     </>
   );
 };
