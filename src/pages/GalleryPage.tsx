a8fd250bc99edd5be46937e05e08d9b8f9ddba01
Ida <ida.nacka@gmail.com>
2024-05-04 21:07:47 +0200

    updated gallery

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -5,9 +5,19 @@ import { useTranslation } from "react-i18next";
 import Catbox from "../assets/catbox.png";
 import TicTacToe from "../assets/tictactoe.png";
 import Webshop from "../assets/webshop.png";
+import { useState } from "react";
 
 export const GalleryPage = () => {
   const [t] = useTranslation("global");
+  const [hoveredBox, setHoveredBox] = useState(null);
+
+  const handleHover = (boxId: any) => {
+    setHoveredBox(boxId);
+  };
+
+  const handleLeave = () => {
+    setHoveredBox(null);
+  };
 
   const boxes = [
     {
@@ -15,18 +25,21 @@ export const GalleryPage = () => {
       image: Catbox,
       title: "CatBox",
       text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga natus quisquam nemo dicta non? Quibusdam nulla et ullam illo at. Doloremque beatae optio dignissimos eum quisquam temporibus ipsa cumque enim. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora eos delectus unde harum hic",
+      link: "https://cat-box.netlify.app/",
     },
     {
       id: 2,
       image: TicTacToe,
       title: "TicTacToe",
       text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga natus quisquam nemo dicta non? Quibusdam nulla et ullam illo at. Doloremque beatae optio dignissimos eum quisquam temporibus ipsa cumque enim. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora eos delectus unde harum hic",
+      link: "https://ida-lindgren-tictactoe.netlify.app/",
     },
     {
       id: 3,
       image: Webshop,
       title: "Webshop",
       text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga natus quisquam nemo dicta non? Quibusdam nulla et ullam illo at. Doloremque beatae optio dignissimos eum quisquam temporibus ipsa cumque enim. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora eos delectus unde harum hic",
+      link: "",
     },
   ];
 
@@ -41,12 +54,32 @@ export const GalleryPage = () => {
         <div className="projects-container">
           <div className="box-container">
             {boxes.map((box) => (
-              <div key={box.id} className="box">
-                <div className="box-container">
-                  <img src={box.image} alt={box.title} className="box-image" />
-                  <h2 className="box-title">{box.title}</h2>
-                  <p className="box-text">{box.text}</p>
-                </div>
+              <div
+                key={box.id}
+                className="box"
+                onMouseEnter={() => handleHover(box.id)}
+                onMouseLeave={handleLeave}
+              >
+                <a
+                  href={box.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="box-a"
+                >
+                  <div
+                    className={`box-container ${
+                      hoveredBox === box.id ? "hovered" : ""
+                    }`}
+                  >
+                    <img
+                      src={box.image}
+                      alt={box.title}
+                      className="box-image"
+                    />
+                    <h2 className="box-title">{box.title}</h2>
+                    <p className="box-text">{box.text}</p>
+                  </div>
+                </a>
               </div>
             ))}
           </div>
