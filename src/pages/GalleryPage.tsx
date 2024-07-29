dee221f60cff604f8c86ed7203b3a72bdbb03638
Ida <ida.nacka@gmail.com>
2024-07-29 23:39:53 +0200

    removed className

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -87,9 +87,7 @@ export const GalleryPage = () => {
       <div className="gallery-wrapper">
         <main className="gallery-container">
           <div className="gallery-header">
-            <h2 className="header-title gallery-h">
-              {t("headers.galleryTitle")}
-            </h2>
+            <h2 className="header-title">{t("headers.galleryTitle")}</h2>
             <p className="gallery-subheading">
               {t("headers.gallerySubheading")}
             </p>
