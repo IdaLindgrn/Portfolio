fdc6b604f8be886bd4aec95d38ca666b6aa58445
Ida <ida.nacka@gmail.com>
2024-11-18 02:52:11 +0100

    fixed text and expertise

--- src/components/MyExpertise.tsx
+++ src/components/MyExpertise.tsx
@@ -12,21 +12,21 @@ export const MyExpertise = () => {
       <div className="expertise-text">
         <div className="expertise-1">
           <img className="expertise-icon" src={Software} alt="Computer icon" />
-          <div>
+          <div className="expertise-div">
             <h3 className="expertise-title">{t("homePage.softDev")}</h3>
             <p className="expertise-info">{t("homePage.softDevText")}</p>
           </div>
         </div>
         <div className="expertise-2">
           <img className="expertise-icon" src={Frontend} alt="React icon" />
-          <div>
+          <div className="expertise-div">
             <h3 className="expertise-title">{t("homePage.frontDev")}</h3>
             <p className="expertise-info">{t("homePage.frontDevText")}</p>
           </div>
         </div>
         <div className="expertise-3">
           <img className="expertise-icon" src={Native} alt="Mobile icon" />
-          <div>
+          <div className="expertise-div">
             <h3 className="expertise-title"> {t("homePage.androidDev")}</h3>
             <p className="expertise-info">{t("homePage.androidDevText")}</p>
           </div>
